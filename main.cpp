111293b7c54effa9928fecc01491e247c7a5998c
Vasiliy <>
2024-07-07 14:18:14 +0300

    added template function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
-static const int width = 2;
+static const int width = 3;
+
+template<class T>
+void calculateSMA(const std::vector<T>& data, std::vector<T>& result, int window);
+
+void print_vector(const std::vector<float>& data);
+
+int main()
+{
+    std::vector<float> src_data = { 2, 4, 6, 8, 12, 14, 16, 18, 20 };
+    std::vector<float> sma_result;
+    calculateSMA(src_data, sma_result, width);
+    print_vector(sma_result);
+
+    return 0;
+}
+
+template<class T>
+void calculateSMA(const std::vector<T>& data, std::vector<T>& result, int window)
+{
+    T sum = 0;
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        sum += data[i];
+        if (i >= window)
+        {
+            sum -= data[i - window];
+        }
+        if (i + 1 >= window)
+        {
+            result.emplace_back(sum / window);
+        }
+    }
+}
 
 void print_vector(const std::vector<float>& data)
 {
@@ -11,26 +43,4 @@ void print_vector(const std::vector<float>& data)
         std::cout << "[" << data[i] << "]";
     }
     std::cout << std::endl;
-}
-
-int main()
-{
-    std::vector<int> src_data = { 2, 4, 6, 8, 12, 14, 16, 18, 20 };
-    std::vector<float> sma_result;
-    int sum = 0;
-    for (size_t i = 0; i < src_data.size(); ++i)
-    {
-        sum += src_data[i];
-        if (i >= width)
-        {
-            sum -= src_data[i - width];
-        }
-        if (i + 1 >= width)
-        {
-            sma_result.emplace_back(sum / width);
-        }
-    }
-    print_vector(sma_result);
-
-    return 0;
 }
