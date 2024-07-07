d0258053a2e9a305641f856268083140b10e2cc2
Vasiliy <>
2024-07-07 18:18:20 +0300

    added negative numbers to random data

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <iomanip>
 using namespace std::chrono;
 
 constexpr int DATA_SIZE = 1000000;
 constexpr int LOWER_BOUND = 0;
-constexpr int UPPER_BOUND = 10000;
-
-const size_t window_width = 4;
+constexpr int UPPER_BOUND = 1000000;
 
 template<class T>
 void calculateSMA(const std::vector<T>& data, std::vector<T>& result, size_t window);
-
-template<class T>
-void print_vector(const std::vector<T>& data);
 
 template<class T>
 std::vector<T> generateRandomData(std::mt19937& random_engine);
@@ -22,6 +18,9 @@ int main()
 {
     std::random_device random_device;
     std::mt19937 engine(random_device());
+
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
 
     std::vector<size_t> window_widths = { 4, 8, 16, 32, 64, 128 };
     std::cout << "Perfomance for float:" << std::endl;
@@ -32,10 +31,10 @@ int main()
         calculateSMA(generateRandomData<float>(engine), float_results, width);
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<milliseconds>(stop - start);
-        std::cout << "Window width " << width << ": " << std::fixed << (DATA_SIZE / (float)duration.count()) * 1000 << std::endl;
+        std::cout << "Window width " << width << ": " << (DATA_SIZE / (float)duration.count()) * 1000 << std::endl;
     }
 
-    std::cout << "Perfomance for float:" << std::endl;
+    std::cout << "Perfomance for double:" << std::endl;
     for (size_t width : window_widths)
     {
         std::vector<double> double_results(DATA_SIZE);
@@ -43,53 +42,40 @@ int main()
         calculateSMA(generateRandomData<double>(engine), double_results, width);
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<milliseconds>(stop - start);
-        std::cout << "Window width " << width << ": " << std::fixed << (DATA_SIZE / (float)duration.count()) * 1000 << std::endl;
+        std::cout << "Window width " << width << ": " << (DATA_SIZE / (float)duration.count()) * 1000 << std::endl;
     }
-
-    std::vector<float> src_data = { 2, 4, 6, 8, 12, 14, 16, 18, 20 };
-    std::vector<float> sma_result(src_data.size());
-    calculateSMA(src_data, sma_result, window_width);
-    print_vector(sma_result);
 
     return 0;
 }
 
 template<class T>
-void calculateSMA(const std::vector<T>& data, std::vector<T>& result, size_t window)
+void calculateSMA(const std::vector<T>& data, std::vector<T>& result, size_t window_width)
 {
     T sum = 0;
-    for (size_t i = 0; i < data.size(); ++i)
+    for (size_t i = 0; i < DATA_SIZE; ++i)
     {
         sum += data[i];
-        if (i >= window)
+        if (i >= window_width)
         {
-            sum -= data[i - window];
+            sum -= data[i - window_width];
         }
-        if (i + 1 >= window)
+        if (i + 1 >= window_width)
         {
-            result[i] = sum / window;
+            result[i] = sum / window_width;
         }
     }
-}
-
-template<class T>
-void print_vector(const std::vector<T>& data)
-{
-    for (int i = 0; i < data.size(); ++i)
-    {
-        std::cout << "[" << data[i] << "]";
-    }
-    std::cout << std::endl;
 }
 
 template<class T>
 std::vector<T> generateRandomData(std::mt19937& random_engine)
 {
-    std::uniform_real_distribution<T> distribution(LOWER_BOUND, UPPER_BOUND);
+    std::uniform_real_distribution<T> distribution(0, std::numeric_limits<T>::max());
+    std::uniform_int_distribution<> int_disctribution(0, 1);
     std::vector<T> data(DATA_SIZE);
     for (size_t i = 0; i < DATA_SIZE; ++i)
     {
-        data[i] = (distribution(random_engine));
+        auto random_value = distribution(random_engine);
+        data[i] = int_disctribution(random_engine) ? random_value : -random_value;
     }
     return data;
 }
